6d814ac9efecc83338372266d9867a0e452a2212
Idris Askarov <id.askarov@gmail.com>
2025-01-14 19:36:21 -0800

    first commit

---
+++ src/main/java/com/codergm/sdj/PersistingAnEntityApplication.java
@@ -0,0 +1,13 @@
+package com.codergm.sdj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistingAnEntityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersistingAnEntityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codergm/sdj/PersistingAnEntityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codergm.sdj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistingAnEntityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
